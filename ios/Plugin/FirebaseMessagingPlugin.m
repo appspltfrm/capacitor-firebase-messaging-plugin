0391e9778168779cf2d495a82750c398b5b6fe37
Mark Chris Levy <levy@codeandmore.pl>
2021-10-03 10:20:13 +0200

    Commit

--- ios/Plugin/FirebaseMessagingPlugin.m
+++ ios/Plugin/FirebaseMessagingPlugin.m
@@ -3,7 +3,7 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(FirebaseMessagingPlugin, "FirebaseMessagingPlugin",
+CAP_PLUGIN(FirebaseMessagingPlugin, "FirebaseMessaging",
     CAP_PLUGIN_METHOD(openNotificationsPermissionSettings, CAPPluginReturnNone);
     CAP_PLUGIN_METHOD(notificationsPermissionState, CAPPluginReturnPromise);
     CAP_PLUGIN_METHOD(subscribeToTopic, CAPPluginReturnPromise);
